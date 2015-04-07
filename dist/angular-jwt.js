5f87b9df9656e91c1203f1031fc68a83c264301e
RoberTu <abc269513055@gmail.com>
2015-04-07 22:06:09 +0800

    fix Base64 decoding problem

--- dist/angular-jwt.js
+++ dist/angular-jwt.js
@@ -61,106 +61,19 @@ angular.module('angular-jwt',
  angular.module('angular-jwt.jwt', [])
   .service('jwtHelper', function() {
 
-    /*
-     * Modification of Vassilis Petroulias's base64.js library
-     * Original notice included below
-     */
-
-    /*
-     Copyright Vassilis Petroulias [DRDigit]
-     Licensed under the Apache License, Version 2.0 (the "License");
-     you may not use this file except in compliance with the License.
-     You may obtain a copy of the License at
-     http://www.apache.org/licenses/LICENSE-2.0
-     Unless required by applicable law or agreed to in writing, software
-     distributed under the License is distributed on an "AS IS" BASIS,
-     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     See the License for the specific language governing permissions and
-     limitations under the License.
-     */
-    this.fromUtf8 = function (s) {
-        /* jshint bitwise:false */
-        var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
-            lookup = null,
-            position = -1,
-            len, buffer = [],
-            enc = [, , , ];
-        if (!lookup) {
-            len = alphabet.length;
-            lookup = {};
-            while (++position < len)
-                lookup[alphabet.charAt(position)] = position;
-            position = -1;
-        }
-        len = s.length;
-        while (++position < len) {
-            enc[0] = lookup[s.charAt(position)];
-            enc[1] = lookup[s.charAt(++position)];
-            buffer.push((enc[0] << 2) | (enc[1] >> 4));
-            enc[2] = lookup[s.charAt(++position)];
-            if (enc[2] === 64)
-                break;
-            buffer.push(((enc[1] & 15) << 4) | (enc[2] >> 2));
-            enc[3] = lookup[s.charAt(++position)];
-            if (enc[3] === 64)
-                break;
-            buffer.push(((enc[2] & 3) << 6) | enc[3]);
-        }
-        return buffer;
-    },
-
     this.urlBase64Decode = function(str) {
-
-      // Replace non-url compatible chars with base64 standard chars
-      var str = str.replace(/-/g, '+').replace(/_/g, '/'),
-          isIEo = /MSIE [67]/.test(navigator.userAgent);
-      // Pad out with standard base64 required padding characters
-      var pad = str.length % 4;
-      if(pad) {
-        if(pad === 1) {
-          throw new Error('InvalidLengthError: Input base64url string is the wrong length to determine padding');
+      var output = str.replace(/-/g, '+').replace(/_/g, '/');
+      switch (output.length % 4) {
+        case 0: { break; }
+        case 2: { output += '=='; break; }
+        case 3: { output += '='; break; }
+        default: {
+          throw 'Illegal base64url string!';
         }
-        str += new Array(5-pad).join('=');
       }
+      return decodeURIComponent(escape(window.atob(output))); //polifyll https://github.com/davidchambers/Base64.js
+    }
 
-      /* jshint bitwise:false */
-      str = str.replace(/\s/g, '');
-      if (str.length % 4){
-          throw new Error('InvalidLengthError: decode failed: The string to be decoded is not the correct length for a base64 encoded string.');
-      }
-      if(/[^A-Za-z0-9+\/=\s]/g.test(str)){
-          throw new Error('InvalidCharacterError: decode failed: The string contains characters invalid in a base64 encoded string.');        
-      }
-
-      var buffer = this.fromUtf8(str),
-          position = 0,
-          output,
-          len = buffer.length;
-
-      if (isIEo) {
-          output = [];
-          while (position < len) {
-              if (buffer[position] < 128)
-                  output.push(String.fromCharCode(buffer[position++]));
-              else if (buffer[position] > 191 && buffer[position] < 224)
-                  output.push(String.fromCharCode(((buffer[position++] & 31) << 6) | (buffer[position++] & 63)));
-              else
-                  output.push(String.fromCharCode(((buffer[position++] & 15) << 12) | ((buffer[position++] & 63) << 6) | (buffer[position++] & 63)));
-          }
-          return output.join('');
-      } else {
-          output = '';
-          while (position < len) {
-              if (buffer[position] < 128)
-                  output += String.fromCharCode(buffer[position++]);
-              else if (buffer[position] > 191 && buffer[position] < 224)
-                  output += String.fromCharCode(((buffer[position++] & 31) << 6) | (buffer[position++] & 63));
-              else
-                  output += String.fromCharCode(((buffer[position++] & 15) << 12) | ((buffer[position++] & 63) << 6) | (buffer[position++] & 63));
-          }
-          return output;
-      }
-    },
 
     this.decodeToken = function(token) {
       var parts = token.split('.');
@@ -168,13 +81,14 @@ angular.module('angular-jwt',
       if (parts.length !== 3) {
         throw new Error('JWT must have 3 parts');
       }
+
       var decoded = this.urlBase64Decode(parts[1]);
       if (!decoded) {
         throw new Error('Cannot decode the token');
       }
 
       return JSON.parse(decoded);
-    },
+    }
 
     this.getTokenExpirationDate = function(token) {
       var decoded;
@@ -188,7 +102,7 @@ angular.module('angular-jwt',
       d.setUTCSeconds(decoded.exp);
 
       return d;
-    },
+    };
 
     this.isTokenExpired = function(token) {
       var d = this.getTokenExpirationDate(token);
@@ -199,8 +113,7 @@ angular.module('angular-jwt',
 
       // Token expired?
       return !(d.valueOf() > new Date().valueOf());
-    }
-
+    };
   });
 
 }());
