86f2b65e74b4b10aff53fad6bd680b3facea01b6
ValeriyRadchenko <radchenkovi@list.ru>
2016-10-10 13:53:30 +0300

    Add requiresLogin: true to authManager
    Add requiresLogin: true to authManager

--- src/angularJwt/services/authManager.js
+++ src/angularJwt/services/authManager.js
@@ -43,6 +43,24 @@ angular.module('angular-jwt.authManager', [])
           unauthenticate();
         });
       }
+      
+      function verifyRoute(event, next) {
+        if (!next) {
+          return false;
+        }
+
+        var routeData = (next.$$route) ? next.$$route : next.data;
+
+        if (routeData && routeData.requiresLogin === true) {
+          if (!$rootScope.isAuthenticated) {
+            config.unauthenticatedRedirector($location);
+            event.preventDefault();
+          }
+        }
+      }
+
+      var eventName = ($injector.has('$state')) ? '$stateChangeStart' : '$routeChangeStart';
+      $rootScope.$on(eventName, verifyRoute);
 
       return {
         authenticate: authenticate,
